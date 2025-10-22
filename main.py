ffcddd1387dfb89c50f13e79bb2137c1b69e9ec8
LevShlyk <97839511+LevShlyk@users.noreply.github.com>
2025-10-22 15:19:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -123,4 +123,25 @@ def main():
 
 
 if __name__ == '__main__':
+    from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# вызываем перед запуском бота
+if __name__ == '__main__':
+    keep_alive()
     main()
+    main()
+
